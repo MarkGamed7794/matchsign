f3406041627f67856263d516e9e1ce3afd2c6461
Mark Metcalfe <markmetcalfe0972@gmail.com>
2024-10-04 15:14:08 -0400

    Rebase to accommodate UI simplification

--- led/display.py
+++ led/display.py
@@ -1,119 +1,41 @@
 #!/usr/bin/env python
 import time
 from led.draw_lib import MatrixDraw, Palette, Fonts
+from ui import UserInterface
 import data_process_2 as data_process
 import constants
 
 DEBUG_MODE = constants.DEBUG_MODE
 
-class UserInterface():
-    def __init__(self, draw: MatrixDraw, Palette: dict, Fonts: dict):
-        self.draw = draw
-        self.Palette = Palette
-        self.Fonts = Fonts
-
-    def MenuSelect(self, header: str, options: list[str]):
-        # fade in animation
-        for box_position in range(self.draw.height, 0, -2):
-            self.draw.rect(0, box_position, self.draw.width, self.draw.height - box_position, self.Palette["black"])
-            self.draw.print(header, 1, box_position+3, self.Palette["white"], self.Fonts["small"])
-            self.draw.flip()
-
-        selected_option = 0
-        while not self.draw.key_just_pressed(constants.BUTTON_SELECT):
-            self.draw.print(header, 1, 5, self.Palette["white"], self.Fonts["small"])
-            for i, option in enumerate(options):
-                self.draw.print(("> " if i == selected_option else "  ") + option, 7, 12 + i * 6, self.Palette["white"], self.Fonts["small"])
-
-
-            if(self.draw.key_just_pressed(constants.BUTTON_UP)):
-                selected_option = (selected_option - 1) % len(options)
-            if(self.draw.key_just_pressed(constants.BUTTON_DOWN)):
-                selected_option = (selected_option + 1) % len(options)
-            self.draw.flip()
-
-        return selected_option
-    
-    def TextEntryPrimitive(self, prompt: str, charset: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 "):
-        # fade in animation
-        for box_position in range(self.draw.height, 0, -2):
-            self.draw.rect(0, box_position, self.draw.width, self.draw.height - box_position, self.Palette["black"])
-            self.draw.print(prompt, 1, box_position+3, self.Palette["white"], self.Fonts["small"])
-            self.draw.flip()
-
-        cursor_pos = 0
-        typed = ""
-        framecount = 0
-        repeat_timer = 0
-
-        SPACING = 4
-        MAX_OFFSET = ((self.draw.width // SPACING) - 1) // 2
-        CENTER = (self.draw.width - SPACING) // 2
-        CHAR_COUNT = len(charset)
-
-        while True:
-            self.draw.print(prompt, 1, 5, self.Palette["white"], self.Fonts["small"])
-            self.draw.print(typed + ("" if framecount <= 30 else "|"), 1, 13, self.Palette["white"], self.Fonts["small"])
-            for offset in range(-MAX_OFFSET, MAX_OFFSET + 1):
-                self.draw.print(
-                    charset[(cursor_pos + offset) % CHAR_COUNT],
-                    CENTER + offset * SPACING, 20,
-                    self.Palette["white"] if offset == 0 else self.Palette["gray"],
-                    self.Fonts["small"]
-                )
-            self.draw.print("^", CENTER, 26, self.Palette["white"], self.Fonts["small"])
-            self.draw.print("[2] INSERT   [8] DELETE   [5] OK", 1, 30, self.Palette["white"], self.Fonts["small"])
-            
-
-            cursor_direction = (-1 if self.draw.is_key_pressed(constants.BUTTON_LEFT) else 0) + (1 if self.draw.is_key_pressed(constants.BUTTON_RIGHT) else 0)
-            if(cursor_direction != 0):
-                repeat_timer += 1
-                if(repeat_timer == 1 or repeat_timer >= 20):
-                    cursor_pos = (cursor_pos + cursor_direction) % CHAR_COUNT
-            else:
-                repeat_timer = 0
-            if(self.draw.key_just_pressed(constants.BUTTON_UP)):
-                typed += charset[cursor_pos]
-            if(self.draw.key_just_pressed(constants.BUTTON_DOWN)):
-                typed = typed[:-1]
-            if(self.draw.key_just_pressed(constants.BUTTON_SELECT)):
-                return typed
-
-            framecount = ((framecount + 1) % 60)
-            self.draw.flip()
-
-    def NumberEntry(self, prompt: str):
-        # fade in animation
-        for box_position in range(self.draw.height, 0, -2):
-            self.draw.rect(0, box_position, self.draw.width, self.draw.height - box_position, self.Palette["black"])
-            self.draw.print(prompt, 1, box_position+3, self.Palette["white"], self.Fonts["small"])
-            self.draw.flip()
-
-        cursor_pos = 0
-        typed = ""
-        framecount = 0
-        repeat_timer = 0
-
-        KEYBOARD = [13, 8, 9, 10, 4, 5, 6, 0, 1, 2]
-
-        while True:
-            self.draw.print(prompt, 1, 5, self.Palette["white"], self.Fonts["small"])
-            self.draw.print(typed + ("" if framecount <= 30 else "|"), 1, 16, self.Palette["white"], self.Fonts["small"])
-            self.draw.print("    [#] DELETE        [A] OK    ", 1, 30, self.Palette["white"], self.Fonts["small"])
-            
-            for n, key in enumerate(KEYBOARD):
-                if(self.draw.key_just_pressed(key)): typed += str(n)
-            if(self.draw.key_just_pressed(14)):
-                typed = typed[:-1]
-            if(self.draw.key_just_pressed(3)):
-                return typed
-
-            framecount = ((framecount + 1) % 60)
-            self.draw.flip()
-
 def main(conn_recieve):
     draw = MatrixDraw()
     current_data = None
+    
+    fonts = {
+        "tiny":  draw.loadFont("led/fonts/miniscule.bdf"),
+        "small": draw.loadFont("led/fonts/tom-thumb.bdf"),
+        "big":   draw.loadFont("led/fonts/5x8.bdf")
+    }
+
+    palette = {
+        "red":     draw.newColor(255,   0,   0),
+        "orange":  draw.newColor(255, 127,   0),
+        "yellow":  draw.newColor(255, 255,   0),
+        "lime":    draw.newColor(127, 255,   0),
+        "green":   draw.newColor(  0, 255,   0),
+        "seafoam": draw.newColor(  0, 255, 127),
+        "cyan":    draw.newColor(  0, 255, 255),
+        "ocean":   draw.newColor(  0, 127, 255),
+        "blue":    draw.newColor(  0,   0, 255),
+        "purple":  draw.newColor(127,   0, 255),
+        "magenta": draw.newColor(255,   0, 255),
+        "pink":    draw.newColor(255,   0, 127),
+
+        "white":   draw.newColor(255, 255, 255),
+        "gray":    draw.newColor(127, 127, 127),
+        "dgray":   draw.newColor( 64,  64,  64),
+        "black":   draw.newColor(  0,   0,   0)
+    }
 
     selected_match = 0
 
