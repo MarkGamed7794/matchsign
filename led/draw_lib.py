b8509cf2452d981dae5125e38b225710642d93d0
Mark Metcalfe <markmetcalfe0972@gmail.com>
2024-11-28 16:28:38 -0500

    Attempt to reduce flicker

--- led/draw_lib.py
+++ led/draw_lib.py
@@ -39,7 +39,7 @@ class MatrixDraw():
             options.hardware_mapping = 'regular'
             options.gpio_slowdown = 4
             options.disable_hardware_pulsing = True
-            options.limit_refresh_rate_hz = 60
+            options.pwm_lsb_nanoseconds = 300
 
             matrix = RGBMatrix(options = options)
 
